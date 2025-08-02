3ab3aa4038f8c529aa91954527ffd42b3b9b684d
Boby Tiwari <bobyt2265@gmail.com>
2025-08-02 10:56:23 +0530

    Revert "Feat/15"

--- src/routes/media/media.index.ts
+++ src/routes/media/media.index.ts
@@ -1,9 +1,8 @@
 import  createApp  from '../../lib/create-app'
-
-import { getMedia, uploadMedia } from './media.routes'
-import { getMediaHandler, uploadMediaHandler } from '@/controllers/media'
+import * as mediaRoutes from './media.routes'
+import * as mediaHandler from '@/controllers/media/index'
 const router = createApp()
-    .openapi(getMedia, getMediaHandler)
-    .openapi(uploadMedia, uploadMediaHandler)
+    .openapi(mediaRoutes.getMedia, mediaHandler.getMediaHandler)
+    .openapi(mediaRoutes.uploadMedia, mediaHandler.uploadMediaHandler)
 
 export default router
--- src/routes/media/media.routes.ts
+++ src/routes/media/media.routes.ts
@@ -1,78 +1,53 @@
 import { createRoute, z } from '@hono/zod-openapi'
 
 export const getMedia = createRoute({
-  tags: ['Media'],
-  path: '/get-media',
-  method: 'get',
-  responses: {
-    200: {
-      content: {
-        'application/json': {
-          schema: z.object({
-            message: z.string(),
-            status: z.string(),
-            success: z.boolean(),
-          }),
+    tags: ['Media'],
+    path: '/get-media',
+    method: 'get',
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: z.object({
+              message: z.string(),
+              status: z.string(),
+              success: z.boolean(),
+            }),
+          },
         },
+        description: 'get media',
       },
-      description: 'get media',
-    },
 
-  },
-}
+    },
+  }
 )
 export const uploadMedia = createRoute({
-  tags: ['Media'],
-  path: '/upload-media',
-  method: 'post',
-  request: {
-    body: {
-      content: {
-        'multipart/form-data': {
-          schema: z.any(),
+    tags: ['Media'],
+    path: '/upload-media',
+    method: 'post',
+    request: {
+      body: {
+        content: {
+          'multipart/form-data': {
+            schema: z.any(),
+          },
         },
-      },
-    }
-  },
-  responses: {
-    200: {
-      content: {
-        'application/json': {
-          schema: z.object({
-            message: z.string(),
-            status: z.string(),
-            success: z.boolean(),
-          }),
+      }
+    },  
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: z.object({
+              message: z.string(),
+              status: z.string(),
+              success: z.boolean(),
+            }),
+          },
         },
+        description: 'upload media',
       },
-      description: 'upload media',
     },
-    400: {
-      content: {
-        'application/json': {
-          schema: z.object({
-            message: z.string(),
-            status: z.string(),
-            success: z.boolean(),
-          }),
-        },
-      },
-      description: 'Bad request',
-    },
-    500: {
-      content: {
-        'application/json': {
-          schema: z.object({
-            message: z.string(),
-            status: z.string(),
-            success: z.boolean(),
-          }),
-        },
-      },
-      description: 'Internal server error',
-    },
-  },
-}
-)
+})
 export type getMedia = typeof getMedia
 export type uploadMedia = typeof uploadMedia
